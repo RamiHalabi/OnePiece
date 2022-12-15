3bb3e0b8a462dbddb865c7b6aad4c0f2ea7f0fda
Rami Halabi <ramiihalabi@outlook.com>
2022-12-15 00:08:20 -0600

    Create homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+// Homepage
 function App() {
+
+  const [number, setNumber] = useState();
+  
+
+  const handleChange = (event) => {
+    const input = event.target.value;
+    const regex = /^\d{0,4}$/; // Regular expression that matches up to 4 digits
+    if (regex.test(input)) {
+      // If the input matches the regular expression, update the value
+      setNumber(input);
+    }
+  }
+
+console.log(number)
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Wrapper'>
+        <h1 className='h1'>One Piece Manga</h1>
+        <input 
+          className='chapterInput' 
+          type="number"
+          min = "1"
+          max = "1090" 
+          onChange={handleChange}
+          placeholder="Enter Chapter #" 
+        />
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/Chapter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Chapter = () => {
+
+
+    return (<>
+    
+    
+    </>);
+    
+};
+
+export default Chapter;
